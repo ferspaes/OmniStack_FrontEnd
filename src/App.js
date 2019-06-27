289b023e48a8c5157ac327bf6dd26123121d12b2
fernando.js8 <fernando.js8@outlook.com>
2019-06-27 07:56:33 -0300

    in progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Routes from './routes';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './componentes/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes />
+    </BrowserRouter>
   );
 }
 

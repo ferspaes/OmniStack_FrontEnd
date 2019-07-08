692622001d8b8500cfcc697f8954326735f6dd96
fernando.js8 <fernando.js8@outlook.com>
2019-07-08 15:52:32 -0300

    Almost there

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,9 +1,64 @@
 import React, { Component } from 'react';
+import './Feed.css';
+import more from '../assets/more.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
+import send from '../assets/send.svg';
 
 class Feed extends Component {
     render(){
         return (
-            <h1>Feed</h1>
+            <section id="post-list">
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Nome do usuario</span>
+                            <span className="place">Rio do Sul</span>
+                        </div>
+                        <img src={more} alt="Mais" className="icon-buttons" />
+                    </header>
+                    <img src="http://localhost:3333/files/zelda.jpg" alt="template inicial" />
+                
+                <footer>
+                    <div className="actions">
+                        <img src={like} alt="Curtir" className="icon-buttons" />
+                        <img src={comment} alt="Comentar" className="icon-buttons" />
+                        <img src={send} alt="Enviar" className="icon-buttons" />
+                    </div>
+
+                    <strong>1000 Likes</strong>
+                    <p>Um post muito top! 
+                        <span className="hashtags"> #FR4Geek #Top! #RedeSocialFR4Geek</span>
+                    </p>
+
+                </footer>
+                </article>
+
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Nome do usuario</span>
+                            <span className="place">Rio do Sul</span>
+                        </div>
+                        <img src={more} alt="Mais" className="icon-buttons" />
+                    </header>
+                    <img src="http://localhost:3333/files/zelda.jpg" alt="template inicial" />
+                
+                <footer>
+                    <div className="actions">
+                        <img src={like} alt="Curtir" className="icon-buttons" />
+                        <img src={comment} alt="Comentar" className="icon-buttons" />
+                        <img src={send} alt="Enviar" className="icon-buttons" />
+                    </div>
+
+                    <strong>1000 Likes</strong>
+                    <p>Um post muito top! 
+                        <span className="hashtags"> #FR4Geek #Top! #RedeSocialFR4Geek</span>
+                    </p>
+
+                </footer>
+                </article>
+            </section>
         );
     };
 }

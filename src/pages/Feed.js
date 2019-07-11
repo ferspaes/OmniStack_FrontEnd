dde9aa0e8cdc306e4d54f42de75f0df3031a2849
fernando.js8 <fernando.js8@outlook.com>
2019-07-10 22:41:06 -0300

    really almost there

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import api from '../services/api';
+import io from 'socket.io-client';
 
 import './Feed.css';
 import more from '../assets/more.svg';
@@ -11,14 +12,45 @@ class Feed extends Component {
     state = {
         feed: [],
     };
+
+    handleLikes = id => {
+        api.post(`/posts/${id}/like`);
+    }
+
+    registerToSocket = () => {
+        const socket = io('http://localhost:3333');
+
+        socket.on('post', newPost => {
+            this.setState({ feed: [newPost, ...this.state.feed] });
+        })
+
+        socket.on('like', LikedPost => {
+            this.setState({
+                feed: this.state.feed.map(post => 
+                    post._id === LikedPost._id ? LikedPost : post
+                )
+            });
+        })
+    }
     
     async componentDidMount(){
+        this.registerToSocket();
+
         const response = await api.get('posts');
 
         this.setState({ feed: response.data });
     };
 
     render(){
+        const months = ["Jan", "Feb", "Mar","Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+        function appendLeadingZeroes(n){
+            if(n <= 9){
+              return "0" + n;
+            }
+            return n
+          }
+
         return (
             <section id="post-list">
                 { this.state.feed.map(post => (
@@ -37,18 +69,38 @@ class Feed extends Component {
                 
                         <footer>
                             <div className="actions">
-                                <img src={like} alt="Curtir" className="icon-buttons" />
+                                <button type="button">
+                                    <img src={like} alt="Curtir" className="icon-buttons" onClick={() => this.handleLikes(post._id)}/>
+                                </button>
                                 <img src={comment} alt="Comentar" className="icon-buttons" />
                                 <img src={send} alt="Enviar" className="icon-buttons" />
                             </div>
 
-                            <strong>{post.likes}</strong>
+                            <strong>{post.likes} Likes</strong>
                         
                             <p>{post.description} 
                                 <span className="hashtags"> #{post.hashtags}</span>
                             </p>
                             <p>
-                                Criado em: {post.createdAt} <br /> Última Atualização: {post.updatedAt}
+                                Publicado em: 
+                                        {
+                                           " " + new Date(post.createdAt).getDate() + " " 
+                                            + months[new Date(post.createdAt).getMonth()] + " " 
+                                            + new Date(post.createdAt).getFullYear() + " at "
+                                            + appendLeadingZeroes(new Date(post.createdAt).getHours()) + ":" 
+                                            + appendLeadingZeroes(new Date(post.createdAt).getMinutes()) + ":" 
+                                            + appendLeadingZeroes(new Date(post.createdAt).getSeconds())
+                                        } 
+                                            <br /> 
+                                Última Atualização: 
+                                        {
+                                            " " + new Date(post.updatedAt).getDate() + " "
+                                            + months[new Date(post.updatedAt).getMonth()] + " "
+                                            + new Date(post.updatedAt).getFullYear() + " at "
+                                            + appendLeadingZeroes(new Date(post.updatedAt).getHours()) + ":" 
+                                            + appendLeadingZeroes(new Date(post.updatedAt).getMinutes()) + ":" 
+                                            + appendLeadingZeroes(new Date(post.updatedAt).getSeconds())
+                                        }
                             </p>
 
                         </footer>
